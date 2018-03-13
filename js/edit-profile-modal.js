d60f07986e367424d8aa39d28d3899c26309ec83
Andhika Satya <andhika@mykios.co>
2018-03-13 11:32:04 +0700

    logged in user at navbar

--- js/edit-profile-modal.js
+++ js/edit-profile-modal.js
@@ -58,6 +58,14 @@ closeDetPort.onclick = function () {
   body.style.overflow = "auto";
 }
 
+var btnUser = document.getElementById("user-action");
+var userActions = document.getElementById("actions");
+var clickCounter = 0;
+
+btnUser.onclick = function () {
+  userActions.style.display = "flex";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modalProfile) {
@@ -72,4 +80,14 @@ window.onclick = function(event) {
     modalDetPort.style.display = "none";
     body.style.overflow = "auto";
   }
+  var style = userActions.style.display;
+    if (userActions.contains(event.target)) {
+    } else {
+      if (clickCounter < 1) {
+        clickCounter++;
+      } else {
+        userActions.style.display = "none";
+        clickCounter = 0;
+      }
+    }
 }
